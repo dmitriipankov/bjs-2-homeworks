606781e276683c16df01d2e6a055f903a79ef9c7
dmitrii pankov <pankovdmitrii@gmail.com>
2026-03-15 20:36:59 +0300

    [200~

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d;
+  let x1;
+  let x2;
+
+  d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  } else if (d > 0) {
+    x1 = (-b + Math.sqrt(d) ) / (2 * a);
+    x2 = (-b - Math.sqrt(d) ) / (2 * a);
+    arr.push(x1, x2);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthPercent = (percent / 100) / 12;
+  let creditBody = amount - contribution;
+  let pay = creditBody * (monthPercent + (monthPercent / (Math.pow((1 + monthPercent), countMonths) - 1)));
+  let totalAmount = Number((pay * countMonths).toFixed(2));
+
+  return totalAmount;
 }
