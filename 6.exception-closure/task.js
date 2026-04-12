b66343b86de331825b6c4c69d426da3ec282f810
dmitrii pankov <pankovdmitrii@gmail.com>
2026-04-12 20:18:19 +0300

    the sixth task is completely ready

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(count) {
+  const result = Number.parseFloat(count);
+  if (!result) {
+    throw new Error("Невалидное значение");
+  }
+  return result;
+}
+
+function validateCount(count) {
+  try {
+    return parseCount(count);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    this.sideA = a;
+    this.sideB = b;
+    this.sideC = c;
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+    return this.sideA + this.sideB + this.sideC;
+  }
+
+  get area() {
+    let p = (this.sideA + this.sideB + this.sideC) / 2;
+    return Number(Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)).toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }
+  }
+}
